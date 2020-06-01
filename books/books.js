326a48d482215737902bbfdd015284a805065eb8
linlinliu <linlinliu03@gmail.com>
2020-05-31 20:52:22 -0700

    Setup main books route

--- books/books.js
+++ books/books.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("This is our main endpoint!")
+})
+
 app.listen(4545, () => console.log("This is our books service!"))
