c644f3c3dd75feda986aa83566cff41f940452f2
linlinliu <linlinliu03@gmail.com>
2020-05-31 22:55:23 -0700

    Add database schema for Book

---
+++ books/Book.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    author: {
+        type: String,
+        required: true
+    },
+     
+    numberPages: {
+        type: Number,
+        required: false
+    },
+
+    publisher: {
+        type: String,
+        required: false
+    }
+})
+
+module.exports = Book = mongoose.model("books", BookSchema);
