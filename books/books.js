23a89bac2dbeea83431ff0df0d4959b7091aa4fb
linlinliu <linlinliu03@gmail.com>
2020-05-31 21:32:48 -0700

    Add database for books service

--- books/books.js
+++ books/books.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb + srv://linda:linda@cluster0-zi67z.mongodb.net/test?retryWrites=true&w=majority", () => {
+    console.log("Connect to database successfully!")
+    }
+)
+
 app.get("/", (req, res) => {
     res.send("This is our main endpoint!")
 })
